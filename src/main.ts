564fee3d7e05e444d77370e39b269968b8323006
BloodPixel <bloodpixellp@gmail.com>
2021-05-07 18:34:03 +0200

    add enemy movement (#5)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Enemy } from "./enemy.js";
 import { player } from "./player.js";
 const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
 	document.getElementById("jkonsInvader")
@@ -10,6 +11,17 @@ const context: CanvasRenderingContext2D = canvas.getContext(
 // Set canvas height and with in JS, because with and height set in CSS distort drawn shapes
 canvas.width = 512;
 canvas.height = 448;
+context.imageSmoothingEnabled = false;
+
+// TODO array of enemies
+const enemy: Enemy = new Enemy(context, 10, 10);
+function animate(): void {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  enemy.moveEnemy();
+  requestAnimationFrame(animate);
+}
+
+animate();
 context.fillStyle = "white";
 context.fillRect(10, 10, 2, 15);
 let p = new player(context);
