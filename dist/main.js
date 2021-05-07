e1f742973eaac0fa3f97ec1c1a4cb651f2ecb9c8
BloodPixel <bloodpixellp@gmail.com>
2021-05-07 14:03:24 +0200

    delete dist on github

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var canvas = (document.getElementById("jkonsInvader"));
-var context = canvas.getContext("2d");
-// Set canvas height and with in JS, because with and height set in CSS distort drawn shapes
-canvas.width = 512;
-canvas.height = 448;
-context.fillStyle = "white";
-context.fillRect(10, 10, 2, 15);
