9452036c0cf8f5be2906d74ad4d198dbd7181696
jim8knopf <jimknopf8jk@gmail.com>
2021-05-07 13:31:05 +0200

    add iro icon

---
+++ src/player.ts
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('jkonsInvader'),
+context = canvas.getContext('2d');
+
+make_base();
+
+function make_base()
+{
+  let base_image = new Image();
+  base_image.src = '../img/iro.png';
+  base_image.onload = function(){
+    context.drawImage(base_image, 0, 0);
+  }
+}
