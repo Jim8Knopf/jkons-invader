354de4cb0056a2edddac873d1b170762b9b762f2
BloodPixel <bloodpixellp@gmail.com>
2021-05-08 15:22:09 +0200

    Enemy -> Master

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { Enemy } from "./enemy.js";
 import { player } from "./player.js";
-import { Enemy } from "./enemy.js";
 import { EnemyHandler } from "./enemyHandler.js";
 
 const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
@@ -16,16 +15,6 @@ canvas.width = 512;
 canvas.height = 448;
 context.imageSmoothingEnabled = false;
 let p = new player(context);
-
-// TODO array of enemies
-const enemy: Enemy = new Enemy(context, 10, 10);
-function animate(): void {
-	context.clearRect(0, 0, canvas.width, canvas.height - p.size);
-	enemy.moveEnemy();
-	requestAnimationFrame(animate);
-}
-
-animate();
 context.fillStyle = "white";
 context.fillRect(10, 10, 2, 15);
 
