87827d82e71e8e85f6e8ebee56cebbfbceb06839
jim8knopf <jimknopf8jk@gmail.com>
2021-05-08 22:40:28 +0200

    soot works

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Enemy } from "./enemy.js";
 import { player } from "./player.js";
 import { EnemyHandler } from "./enemyHandler.js";
+import { shoot } from "./shoot.js";
 
 const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
 	document.getElementById("jkonsInvader")
@@ -14,7 +15,7 @@ const context: CanvasRenderingContext2D = canvas.getContext(
 canvas.width = 512;
 canvas.height = 448;
 context.imageSmoothingEnabled = false;
-let p = new player(context);
+let p: player = new player(context);
 context.fillStyle = "white";
 context.fillRect(10, 10, 2, 15);
 
@@ -23,23 +24,23 @@ function unloadPage() {
 	document.removeEventListener("keydown", function (event) {
 		p.move(event);
 	});
-	console.log("test");
-	setTimeout(() => {}, 5000);
 }
 context.imageSmoothingEnabled = false;
 
 const enemyHandler: EnemyHandler = new EnemyHandler();
-// ! Should not be, but dummy enemy for zoom and tile size, till game settings and tile config is created. 
-const enemy: Enemy = new Enemy(context, -100 , -100);
+// ! Should not be, but dummy enemy for zoom and tile size, till game settings and tile config is created.
+const enemy: Enemy = new Enemy(context, -100, -100);
 const spaceBetween = enemy.getZoom() * enemy.getTileWidth();
 for (let i = 0; i < 6; i++) {
-	enemyHandler.addEnemy(new Enemy(context, i * spaceBetween , enemy.getTileHeight()));
-	
+	enemyHandler.addEnemy(
+		new Enemy(context, i * spaceBetween, enemy.getTileHeight())
+	);
 }
 
 function animate(): void {
 	setTimeout(() => {
 		enemyHandler.moveEnemies();
+		p.shoot.shootMovement();
 		requestAnimationFrame(animate);
 	}, 1000 / 120);
 }
---
+++ src/shoot.ts
@@ -0,0 +1,69 @@
+export class shoot {
+	public positionX: number;
+	public positionY: number;
+	private _context: CanvasRenderingContext2D;
+	private _shootLength: number = 10;
+	private _shootThickness: number = 5;
+	private _shootSpeed: number = 10;
+	constructor(context: CanvasRenderingContext2D) {
+		this._context = context;
+		this.positionX = 0;
+		this.positionY = 0;
+	}
+	/**
+	 * shoot if there is no actual shoot an the field
+	 */
+	public shoot(positionX: number, positionY: number) {
+		if (this.positionY <= 0) {
+			this._clear();
+			this.positionX = positionX;
+			this.positionY = positionY;
+		}
+	}
+	/**
+	 * hit should be called when anything is hitten by the shot
+	 */
+	public hit() {
+		this._clear();
+		this.positionY = 0;
+	}
+	/**
+	 * this function checks if the shoot is in the field
+	 * than it clears the old shoot
+	 * than it moves the shoot up and paint it
+	 */
+	public shootMovement() {
+		if (this.positionY <= 0) {
+			this.positionY = 0;
+			// this.clear();
+			return;
+		}
+		this._clear();
+		this.positionY -= this._shootSpeed;
+		this._shootRender();
+	}
+
+	/**
+	 * shootRender
+	 */
+	private _shootRender() {
+		this._context.fillStyle = "red";
+		this._context.fillRect(
+			this.positionX - this._shootThickness / 2,
+			this.positionY - this._shootLength,
+			this._shootThickness,
+			this._shootLength
+		);
+	}
+	/**
+	 * this function clears the current shoot
+	 */
+	private _clear() {
+		this._context.clearRect(
+			this.positionX - this._shootThickness / 2,
+			this.positionY - this._shootLength,
+			this._shootThickness + 1,
+			this._shootLength + 1
+		);
+	}
+}
