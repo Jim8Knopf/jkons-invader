377e8f7f2bdb5cdd041de18b70a03440c483421c
jim8knopf <jimknopf8jk@gmail.com>
2021-05-19 00:43:58 +0200

    add reset function

--- src/main.ts
+++ src/main.ts
@@ -15,30 +15,31 @@ let animationActive: boolean = true;
 let animationSpeed: number = 1 / 60;
 
 let gameStarted: boolean = false;
-
 const player = newPlayer("a", "d", " ");
-const enemyCorp = new EnemyCorp(16, 4);
-export function init() {
-	document.addEventListener("keyup", (keyboard) => {
-		switch (keyboard.key) {
-			case "r":
-				if (gameStarted === false) {
-					playTitleTheme();
-					gameStarted = true;
-					animate();
-				} else {
-					init();
-				}
-				break;
+const gameOverImage = new Image();
+gameOverImage.src = "assets/img/game_over.png";
+let enemyCorp = new EnemyCorp(16, 4);
 
-			default:
-				gameStarted = false;
-				break;
-		}
-	});
-}
 loadScoreboard();
 enemyCorp.corpAnimation();
+init();
+
+function reload() {
+	enemyCorp = new EnemyCorp(16, 4);
+	enemyCorp.corpAnimation();
+}
+
+function _renderGameOver() {
+	let x = (getCanvas().width - (71 * getScaledTileSize()) / 9) / 2;
+	let y = (getCanvas().height - (33 * getScaledTileSize()) / 9) / 2;
+	getContext().drawImage(
+		gameOverImage,
+		x,
+		y,
+		(71 * getScaledTileSize()) / 9,
+		(33 * getScaledTileSize()) / 9
+	);
+}
 function animate(): void {
 	setTimeout(() => {
 		player.handleInput();
@@ -51,6 +52,44 @@ function animate(): void {
 		}
 	}, animationSpeed);
 }
+
+function input(keyboard: KeyboardEvent) {
+	switch (keyboard.key) {
+		case "r":
+			if (!gameStarted) {
+				playTitleTheme();
+				gameStarted = true;
+				animationActive = true;
+				animate();
+			}
+			break;
+		case "p":
+			cancelAnimationFrame(animation);
+			getContext().clearRect(
+				0,
+				0,
+				getContext().canvas.width,
+				getContext().canvas.height
+			);
+			stopTitleTheme();
+			gameStarted = false;
+			animationActive = false;
+			enemyCorp = new EnemyCorp(16, 4);
+			animate();
+
+			break;
+
+		default:
+			gameStarted = false;
+			break;
+	}
+}
+
+export function init() {
+	document.addEventListener("keyup", (keyboard) => {
+		input(keyboard);
+	});
+}
 export function stopGame() {
 	animationActive = false;
 	cancelAnimationFrame(animation);
@@ -59,18 +98,3 @@ export function stopGame() {
 	playGameOverMusic();
 	displayForm();
 }
-const gameOverImage = new Image();
-gameOverImage.src = "assets/img/game_over.png";
-function _renderGameOver() {
-	let x = (getCanvas().width - (71 * getScaledTileSize()) / 9) / 2;
-	let y = (getCanvas().height - (33 * getScaledTileSize()) / 9) / 2;
-	getContext().drawImage(
-		gameOverImage,
-		x,
-		y,
-		(71 * getScaledTileSize()) / 9,
-		(33 * getScaledTileSize()) / 9
-	);
-}
-
-init();
